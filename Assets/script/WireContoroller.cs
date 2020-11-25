7f259a488b8628ef2aa8e0400dc9788e6751f7d1
ShinNakajima128 <ukauka224@i.softbank.jp>
2020-11-25 15:53:02 +0900

    gitの準備が終了した

---
+++ Assets/script/WireContoroller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WireContoroller : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private RaycastHit hit;
+    private Ray ray;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+    void RayTest()
+    {
+        //Rayの作成　　　　　　　↓Rayを飛ばす原点　　　↓Rayを飛ばす方向
+        Ray ray = new Ray(transform.position, new Vector3(0, -1, 0));
+
+        //Rayが当たったオブジェクトの情報を入れる箱
+        RaycastHit hit;
+
+        //Rayの飛ばせる距離
+        int distance = 10;
+
+        //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの色
+        Debug.DrawLine(ray.origin, ray.direction * distance, Color.red);
+
+        //もしRayにオブジェクトが衝突したら
+        //                  ↓Ray  ↓Rayが当たったオブジェクト ↓距離
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            //Rayが当たったオブジェクトのtagがPlayerだったら
+            if (hit.collider.tag == "Target")
+                Debug.Log("RayがTargetに当たった");
+        }
+    }
+}
+//int centerX = Screen.width / 2;
+//int centerY = Screen.height / 2;
+
+//Vector3 pos = new Vector3(centerX, centerY, 0.1f); // Zを少しだけ前に出す
+//Ray ray = Camera.main.ScreenPointToRay(pos);
