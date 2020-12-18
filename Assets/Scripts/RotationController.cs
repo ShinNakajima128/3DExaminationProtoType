939fb20d383f2687d758ef905ed691ddb695da1d
ShinNakajima128 <ukauka224@i.softbank.jp>
2020-12-18 17:57:12 +0900

    Mainにてステージを作成。ステージを装飾するAssetの追加

---
+++ Assets/Scripts/RotationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+    [SerializeField] float m_rotateX = 0.5f;
+    [SerializeField] float m_rotateY = 0.5f;
+    [SerializeField] float m_rotateZ = 0.5f;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(m_rotateX, m_rotateY, m_rotateZ));
+    }
+}
---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    Rigidbody m_rb;
+    [SerializeField] GameObject m_exprosion;
+
+    void Start()
+    {
+        m_rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(m_exprosion);
+        Destroy(this.gameObject);
+    }
+}
