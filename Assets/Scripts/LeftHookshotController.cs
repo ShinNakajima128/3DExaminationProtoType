e8299ea9d9f99adc9b8a30d67286a0a25aec80ea
ShinNakajima128 <ukauka224@i.softbank.jp>
2020-12-30 00:55:43 +0900

    ターゲットが照準に入った時に照準を変化させ、ターゲットが入ってない時は元に戻すScriptを追加

--- Assets/Scripts/LeftHookshotController.cs
+++ Assets/Scripts/LeftHookshotController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeftHookshotController : MonoBehaviour
 {
@@ -10,20 +11,26 @@ public class LeftHookshotController : MonoBehaviour
     [SerializeField] Transform m_player;
     [SerializeField] AudioClip m_flyingSfx = null;
     [SerializeField] AudioClip m_hookHitSfx = null;
-    [SerializeField] float m_maxDistance = 100f;
+    [SerializeField] float m_maxDistance = 40f;
+    [SerializeField] GameObject m_reticle;
+    Image reticleImage;
     LineRenderer lr;
     SpringJoint joint;
     Vector3 hookPoint;
     HookshotController hookshotController;
-    
-    void Awake()
+    RaycastHit reticleHit;
+
+
+    void Start()
     {
         lr = GetComponent<LineRenderer>();
         hookshotController = GetComponent<HookshotController>();
+        reticleImage = m_reticle.GetComponent<Image>();
     }
-
     void Update()
     {
+        ReticleHitAnim();
+       
         if (Input.GetKeyDown(KeyCode.Joystick1Button4))
         {
             hookshotController.enabled = false;
@@ -78,5 +85,31 @@ public class LeftHookshotController : MonoBehaviour
         if (!joint) return;
         lr.SetPosition(0, m_muzzle.position);
         lr.SetPosition(1, hookPoint);
+    }
+
+    void ReticleHitAnim()
+    {
+        if (Physics.Raycast(m_camera.position, m_camera.forward, out reticleHit, m_maxDistance))
+        {
+            string hittag = reticleHit.collider.tag;
+
+            if (hittag.Equals("HookTarget"))
+            {
+                reticleImage.color = new Color(1.0f, 0, 0, 1.0f);
+                m_reticle.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            }
+        }
+        else if (Physics.Raycast(m_camera.position, m_camera.forward, out reticleHit, 200f))
+        {
+            string hittag = reticleHit.collider.tag;
+
+            if (hittag.Equals("Stage"))
+            {
+                reticleImage.color = new Color(0, 1.0f, 1.0f, 1.0f);
+                m_reticle.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            }
+        }
+
+
     }
 }
