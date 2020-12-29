a8db4ee2f1c1f9e04bb633c594769b9e3f047491
ShinNakajima128 <ukauka224@i.softbank.jp>
2020-12-29 14:18:42 +0900

    Sceneのロードが開始したらフェードアウトするScriptを作成

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -9,31 +9,53 @@ public class TitleManager : MonoBehaviour
     [SerializeField] GameObject m_menuText = null;
     [SerializeField] AudioClip m_decisionSfx = null;
     [SerializeField] AudioClip m_backSfx = null;
+    [SerializeField] float m_SceneLoadTime = 2f;
+    [SerializeField] GameObject m_fadeController;
+    bool isLoadStarted = false;
+    FadeController FC;
     AudioSource audioSource;
     private float m_timer;
 
-    private void Awake()
+    private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        FC = m_fadeController.GetComponent<FadeController>();
+        FC.isFadeIn = true;
     }
+
     void Update()
     {
+        Debug.Log(m_timer);
         if (Input.anyKeyDown && m_pressAnyButtonText.activeSelf == true)
         {
             audioSource.PlayOneShot(m_decisionSfx);
             m_pressAnyButtonText.SetActive(false);
             m_menuText.SetActive(true);
         }
-        else if (m_pressAnyButtonText.activeSelf == false && Input.GetButtonDown("Jump"))
+        else if (m_pressAnyButtonText.activeSelf == false)
         {
-            audioSource.PlayOneShot(m_backSfx);
-            m_pressAnyButtonText.SetActive(true);
-            m_menuText.SetActive(false);
+            if (Input.GetButtonDown("Jump")) 
+            {
+                audioSource.PlayOneShot(m_backSfx);
+                m_pressAnyButtonText.SetActive(true);
+                m_menuText.SetActive(false);
+            }
+            else if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+            {
+                FC.isFadeOut = true;
+                audioSource.PlayOneShot(m_decisionSfx);
+                isLoadStarted = true;
+            }
         }
-        else if (m_pressAnyButtonText.activeSelf == false && Input.GetKeyDown(KeyCode.Joystick1Button1))
+        if (isLoadStarted)
         {
-            audioSource.PlayOneShot(m_decisionSfx);
-            SceneManager.LoadScene("Tutorial");
+            m_timer += Time.deltaTime;
+
+            if (m_timer >= m_SceneLoadTime)
+            {
+                m_timer = 0;
+                SceneManager.LoadScene("Tutorial");
+            }
         }
     }
 }
