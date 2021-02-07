bc645138667dbc8f866491cafecdf7e3cac96293
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-02-08 01:52:27 +0900

    メニューの項目を選択するとSceneが選択したSceneへ遷移するようにした

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,24 +11,20 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject m_UI;
     [SerializeField] GameObject m_menuUI;
     [SerializeField] GameObject m_fadeController;
-    [SerializeField] AudioClip m_MenuSfx;
+    [SerializeField] AudioClip m_menuSfx;
+    [SerializeField] AudioClip m_selectSfx;
     FadeController FC;
     AudioSource audioSource;
+    float m_timer;
+    int m_SceneLoadTime = 2;
+    int loadType;
     bool isLoadStarted = false;
-    State menuState;
-    int stateCount;
-
-    enum State
-    {
-        Restart,
-        StageSelect,
-        Exit
-    }
 
     void Start()
     {
         FC = m_fadeController.GetComponent<FadeController>();
         audioSource = GetComponent<AudioSource>();
+        FC.isFadeIn = true;
     }
 
     void Update()
@@ -36,51 +32,56 @@ public class GameManager : MonoBehaviour
         if (m_UI.activeSelf && Input.GetKeyDown(KeyCode.JoystickButton2))
         {
             Debug.Log("Xが押されました");
-            audioSource.PlayOneShot(m_MenuSfx);
+            audioSource.PlayOneShot(m_menuSfx);
             m_UI.SetActive(false);
-            m_menuUI.GetComponent<Canvas>().enabled = true;
-
-            if (Input.GetKeyDown(KeyCode.Joystick1Button1))
-            {
-                menuState = State.Restart;
-            }
+            m_menuUI.GetComponent<Canvas>().enabled = true;  
         }
         else if (!m_UI.activeSelf && Input.GetKeyDown(KeyCode.JoystickButton2))
         {
-            audioSource.PlayOneShot(m_MenuSfx);
+            audioSource.PlayOneShot(m_menuSfx);
             m_UI.SetActive(true);
             m_menuUI.GetComponent<Canvas>().enabled = false;
         }
     }
 
-    private void FixedUpdate()
+    public void Restart()
     {
-        switch (menuState)
+        audioSource.PlayOneShot(m_selectSfx);
+        FC.isFadeOut = true;
+        loadType = 1;
+        StartCoroutine(loadTimer());
+    }
+
+    public void StageSelect()
+    {
+        audioSource.PlayOneShot(m_selectSfx);
+        FC.isFadeOut = true;
+        loadType = 2;
+        StartCoroutine(loadTimer());
+    }
+
+    public void GameExit()
+    {
+        audioSource.PlayOneShot(m_selectSfx);
+        FC.isFadeOut = true;
+        loadType = 3;
+        StartCoroutine(loadTimer());
+    }
+    IEnumerator loadTimer()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        if (loadType == 1)
         {
-            default:
-            case State.Restart:
-                Restart();
-                break;
-            case State.StageSelect:
-                StageSelect();
-                break;
-            case State.Exit:
-                GameExit();
-                break;
+            SceneManager.LoadScene("Stage1");
         }
-    }
-    void Restart()
-    {
-
-    }
-
-    void StageSelect()
-    {
-
-    }
-
-    private void GameExit()
-    {
-        
+        else if (loadType == 2)
+        {
+            SceneManager.LoadScene("Stage2");
+        }
+        else if (loadType == 3)
+        {
+            SceneManager.LoadScene("Title");
+        }
     }
 }
