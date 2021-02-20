90ebd22d2b40306edb6d7ec342157857a9f50ce1
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-02-20 20:04:43 +0900

    HighJumpItemを使用すると高くジャンプするようにした

--- Assets/Scripts/Items/HighJumpItem.cs
+++ Assets/Scripts/Items/HighJumpItem.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class HighJumpItem : ItemBase
 {
+    [SerializeField] Rigidbody m_player = null;
+    [SerializeField] float m_jumpPower = 30f;
+
     public override void Use()
     {
         Debug.Log("高くジャンプしました");
+        m_player.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
         base.Use();
     }
 }
