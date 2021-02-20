d2ec3826eb18c0aa9c283150ccd924f1b06c7c48
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-02-20 18:01:32 +0900

    3種類のアイテムを作成した

---
+++ Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// アイテムの共通処理を実装したクラス
+/// アイテムはこのクラスを継承して作ること
+/// </summary>
+public class ItemBase : MonoBehaviour
+{
+    [SerializeField] AudioClip m_sfx = null;
+    Vector3 m_swapPoint = new Vector3(0, -100, 0);
+
+    /// <summary>アイテムを手に入れた時に呼ばれる</summary>
+    public void Get()
+    {
+        // 見えない所にアイテムを隠す
+        this.transform.position = m_swapPoint;
+    }
+
+    /// <summary>アイテムが捨てられた時に呼ばれる</summary>
+    public void Throw(Vector3 position)
+    {
+        // 引数で指定された場所にアイテムを出現させる
+        this.transform.position = position;
+    }
+
+    /// <summary>
+    /// アイテムを使う時の共通処理
+    /// </summary>
+    public virtual void Use()
+    {
+        // 効果音を鳴らし、アイテムをすぐに破棄する
+        AudioSource.PlayClipAtPoint(m_sfx, Camera.main.transform.position);
+        DestroyImmediate(this.gameObject);
+    }
+}
