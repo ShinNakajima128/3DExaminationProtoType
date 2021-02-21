2c97d22192e0da714bbdf8ce7078801601dca7e2
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-02-21 14:42:12 +0900

    Scriptフォルダの整頓、動く床をDOTweenで作成した

---
+++ Assets/Scripts/MovingFloorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingFloorController : MonoBehaviour
+{
+    [SerializeField] float m_MoveTime = 2f;
+    [SerializeField] Transform m_target = null;
+    Vector3 m_originPosition;
+
+    void Start()
+    {
+        m_originPosition = this.transform.position;
+        MoveFloor();
+    }
+
+    void MoveFloor()
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(this.transform.DOMove(m_target.position, m_MoveTime).SetEase(Ease.Linear))
+           .AppendInterval(1)
+           .Append(this.transform.DOMove(m_originPosition, m_MoveTime).SetEase(Ease.Linear))
+           .AppendInterval(1)
+           .SetLoops(-1);
+    }
+}
