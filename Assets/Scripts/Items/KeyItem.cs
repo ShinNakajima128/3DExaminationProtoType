8fd6fe85da1f05ae934f7e3de235c44315846870
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-02-23 15:37:52 +0900

    アイテムのブレイカーを使用した時の挙動を変更した。

--- Assets/Scripts/Items/KeyItem.cs
+++ Assets/Scripts/Items/KeyItem.cs
@@ -5,18 +5,11 @@ using UnityEngine;
 public class KeyItem : ItemBase
 {
     [SerializeField] Transform m_throwPosition;
-    [SerializeField] float m_throwPower = 10f;
-    Rigidbody m_rb;
-    void Start()
-    {
-        m_rb = GetComponent<Rigidbody>();
-    }
+    
     public override void Use()
     {
         Debug.Log("鍵を使用しました");
         this.transform.position = m_throwPosition.position;
-        m_rb.useGravity = true;
-        m_rb.AddForce(transform.forward * m_throwPower, ForceMode.Impulse);
     }
 
     void OnCollisionEnter(Collision collision)
