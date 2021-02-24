ab5888b2a9a1d584f8dddee4eec314b8870b77d2
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-02-24 15:00:56 +0900

    近くに行くとKeepOutをフェードインさせ、離れたらフェードアウトする処理のテストSceneを作成した。Stage1とStage2のオブジェクトの配置を変更した。

---
+++ Assets/FadeTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FadeTest : MonoBehaviour
+{
+    [SerializeField] Transform m_target;
+    [SerializeField] float m_duration = 1f;
+    Vector3 m_originPosition;
+    Image m_image = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_image = GetComponent<Image>();
+        m_originPosition = this.transform.position;
+        Keepout();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fade(0);
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            Fade(1);
+        }
+    }
+
+    void Fade(float targetAlpha)
+    {
+        Sequence s = DOTween.Sequence();
+        s.Join(DOTween.ToAlpha(
+            () => m_image.color,
+            color => m_image.color = color,
+            targetAlpha,
+            1.0f));
+        s.Play();
+    }
+
+    void Keepout()
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(this.transform.DOMove(m_target.position, m_duration).SetEase(Ease.Linear))
+            .SetLoops(-1);
+        s.Play();
+    }
+}
--- Assets/Scripts/KeepoutController.cs
+++ Assets/Scripts/KeepoutController.cs
@@ -6,7 +6,7 @@ using DG.Tweening;
 
 public class KeepoutController : MonoBehaviour
 {
-    [SerializeField] GameObject m_keepout;
+    //[SerializeField] GameObject m_keepout;
     [SerializeField] Animator m_anim;
     [SerializeField] Image m_keepImage;
     AudioSource audioSource;
@@ -15,7 +15,7 @@ public class KeepoutController : MonoBehaviour
 
     void Start()
     {
-        m_keepout.SetActive(true);
+        //m_keepout.SetActive(true);
         audioSource = GetComponent<AudioSource>();
         //m_anim.Play("Keepout");
         //m_keepImage.color = new Color(0, 0, 0, 0);
@@ -32,9 +32,12 @@ public class KeepoutController : MonoBehaviour
             audioSource.Play();
             //m_keepout.SetActive(true);
             //m_anim.Play("ON");
-            
+
             //isinvisible = true;
 
+            //m_keepImage.DOFade(0, 1);
+
+            Fade(1);
             //Sequence seq = DOTween.Sequence();
 
             //seq.Append(m_keepImage.DOFade(1, 1));
@@ -51,9 +54,26 @@ public class KeepoutController : MonoBehaviour
             //m_anim.Play("OFF");
             //isinvisible = false;
 
+            Fade(0);
+            //m_keepImage.DOFade(0, 1);
             //Sequence seq = DOTween.Sequence();
             //seq.Append(m_keepImage.DOFade(0, 1));
             //seq.Play();
         }
+    }
+
+    void Fade(float targetAlpha)
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(DOTween.ToAlpha(
+            () => m_keepImage.color,
+            color => m_keepImage.color = color,
+            targetAlpha,
+            1.0f));
+
+        //s.Append(this.transform.DOMove(m_target.position, 1).SetEase(Ease.Linear))
+        //    .Append(this.transform.DOMove(m_target.position, 1).SetEase(Ease.Linear))
+        //    .SetLoops(-1);
+        s.Play();
     }
 }
