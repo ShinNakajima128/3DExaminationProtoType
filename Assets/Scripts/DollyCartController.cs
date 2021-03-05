82cc03bfb3903740dc5229c0215fbc888261b185
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-03-05 23:38:05 +0900

    レールの近くにいくとレールに乗って進むようにしたが、改良の余地あり

---
+++ Assets/Scripts/DollyCartController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DollyCartController : MonoBehaviour
+{
+    CinemachineDollyCart dollyCart;
+
+    void Start()
+    {
+        dollyCart = this.gameObject.GetComponent<CinemachineDollyCart>();
+        dollyCart.m_Speed = 0f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.SetParent(this.transform);
+            dollyCart.m_Speed = 15f;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.SetParent(null);
+        }
+    }
+}
