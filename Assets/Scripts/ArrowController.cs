bdf0c4e600ee42f0a758ff1c8b72c84524dba4a4
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-03-13 07:00:33 +0900

    FreeLookCameraのDeselTimeの値を変更した。進行方向を分かりやすくするためにガイド用の矢印を作成した。

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ArrowController : MonoBehaviour
+{
+    [SerializeField] float m_fadeDuration = 1f;
+    [SerializeField] Image m_arrowImage = null;
+
+    void Start()
+    {
+        Fade(0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Player が接触したら徐々に表示する
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Fade(1);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Player が接触したら徐々に消える
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Fade(0);
+        }
+    }
+
+    void Fade(float targetAlpha)
+    {
+        Sequence s = DOTween.Sequence();
+        s.Join(DOTween.ToAlpha(
+            () => m_arrowImage.color,
+            color => m_arrowImage.color = color,
+            targetAlpha,
+            m_fadeDuration));
+        s.Play();
+    }
+}
