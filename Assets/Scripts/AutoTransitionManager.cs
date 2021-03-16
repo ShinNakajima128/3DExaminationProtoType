c52ad743634363226046e6e5065a3b8e77263efc
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-03-16 22:05:01 +0900

    AutoTransitionManagerにコメントを記入した。

--- Assets/Scripts/AutoTransitionManager.cs
+++ Assets/Scripts/AutoTransitionManager.cs
@@ -6,21 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class AutoTransitionManager : MonoBehaviour
 {
+    /// <summary> 自動的にSceneを遷移するまでの時間を表示するText </summary>
     [SerializeField] Text m_autoTransitionTime = null;
+    /// <summary> 自動的にSceneを遷移するまでの時間 </summary>
     [SerializeField] float m_taransitionTimer = 20f;
+    /// <summary> 時間が切れたら画面をフェードさせるController </summary>
     [SerializeField] FadeController FC;
+    /// <summary> ロードする時に鳴らすSE </summary>
     [SerializeField] AudioClip m_loadSfx = null;
+    /// <summary> タイマーを減らすか否か </summary>
     bool isSubtraction = true;
+    /// <summary> ロードが始まったか否か </summary>
     bool isLoaded = true;
+    /// <summary> ロードする時に鳴らすためのAudioSource </summary>
     AudioSource audiosource;
 
     void Start()
     {
         audiosource = GetComponent<AudioSource>();
     }
+
     void Update()
     {
         m_autoTransitionTime.text = m_taransitionTimer.ToString("F0");
+
         if (isSubtraction)
         {
             m_taransitionTimer -= Time.deltaTime;
