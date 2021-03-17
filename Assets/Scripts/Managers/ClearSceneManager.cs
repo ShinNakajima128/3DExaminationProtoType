fea0dafa6bef94ebd1a6da1cbe369121727f5629
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-03-17 21:59:24 +0900

    ClearSceneManagerにコメントを記入した。

--- Assets/Scripts/Managers/ClearSceneManager.cs
+++ Assets/Scripts/Managers/ClearSceneManager.cs
@@ -6,14 +6,23 @@ using UnityEngine.Playables;
 
 public class ClearSceneManager : MonoBehaviour
 {
+    /// <summary> クリア時に表示するメニューのUI </summary>
     [SerializeField] GameObject m_clearMenuUI;
+    /// <summary> メニューを表示した時にSelect()するButton </summary>
     [SerializeField] Button m_menuButton;
+    /// <summary> ステージを選択するメニューのUI </summary>
     [SerializeField] GameObject m_stageSelectMenuUI;
+    /// <summary> ステージセレクトメニューを表示した時にSelect()するButton </summary>
     [SerializeField] Button m_stageSelectButton;
+    /// <summary> ClearSceneで最初に再生するTimeline </summary>
     [SerializeField] PlayableDirector m_director;
+    /// <summary> ClearSceneを管理しているManagerで表示するUI </summary>
     [SerializeField] GameObject m_resultUI;
+    /// <summary> 自動でTitleに遷移する時間を表示するUI </summary>
     [SerializeField] GameObject m_AutoTransitionUI = null;
+    /// <summary> Timelineの再生が終了したかを確認する変数 </summary>
     int m_state = 0;
+    /// <summary> Timelineが再生中か否か </summary>
     bool isStartPlay = true;
 
     void Update()
